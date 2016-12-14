21c2c646a6ae251cec37e9b7863b032681657d31
Stempkovski <stempkovski8859@saskpolytech.ca>
2016-12-14 12:48:51 -0600

    Hello World Complete

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -17,7 +17,7 @@ public class JavaHelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello World");
+        System.out.println("Hello Brave New World!");
         
     }
     
